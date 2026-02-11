72d3f9396c958ca019439bdffc1a872fddcfd109
Victor Sanjaya <166146650+victorsanjaya27@users.noreply.github.com>
2026-02-11 21:39:19 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+onload = () => {
+  document.body.classList.remove("container");
+};
+
+const wrapper = document.querySelector(".wrapper");
+const openBtn = document.getElementById("openBtn");
+const closeBtn = document.getElementById("closeBtn");
+
+openBtn.addEventListener("click", () => {
+  wrapper.classList.add("open");
+  openBtn.style.display = "none";
+  closeBtn.style.display = "inline-block";
+});
+
+closeBtn.addEventListener("click", () => {
+  wrapper.classList.remove("open");
+  closeBtn.style.display = "none";
+  openBtn.style.display = "inline-block";
+});
+
+// Aktifkan suara saat user klik pertama kali
+document.addEventListener(
+  "click",
+  function () {
+    const music = document.getElementById("bgMusic");
+
+    if (music) {
+      music.muted = false;
+      music.play();
+    }
+  },
+  { once: true },
+);
